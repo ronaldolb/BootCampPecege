74e43f0b71264cd89131589647355bd35876c540
Ronaldo Lopes de Barros <142457971+ronaldolb@users.noreply.github.com>
2025-03-20 21:36:08 -0300

    Modificado

--- obj/Debug/net8.0/ClinAgenda.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClinAgenda.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinAgenda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62bf3f8da66eb9e30b68d9cc72ef02caf604453f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0717b9a59d5b0ee9fbebf9ccd7b30ac6996e1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinAgenda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinAgenda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
